22318b11d88e6c18bf0a0237d33889a72ce08c31
Timon <timon.pitzschel@gmail.com>
2025-08-26 15:46:30 +0200

    Initial commit

---
+++ src/test/java/com/eventplaner/EventPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
