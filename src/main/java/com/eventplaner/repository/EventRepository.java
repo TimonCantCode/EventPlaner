c1b1d7ef01fbbd8c23d80c9f076ba54430f20e21
Timon <timon.pitzschel@gmail.com>
2025-09-04 16:49:05 +0200

    Can register and unregister from Events now, also shown then in My Events Tab - Logs & Css missing, then finished for Release 1

--- src/main/java/com/eventplaner/repository/EventRepository.java
+++ src/main/java/com/eventplaner/repository/EventRepository.java
@@ -1,5 +1,6 @@
 package com.eventplaner.repository;
 
+import com.eventplaner.model.UserProfile;
 import com.eventplaner.model.event.Event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findByAuthor_UserNameOrderByCreatedAtDesc(String userName);
+    List<Event> findByParticipantsContaining(UserProfile userProfile);
 }
